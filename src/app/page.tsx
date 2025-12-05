9546f009a94a4fdd8aff675808c430f4bd1cf9c1
王林 <wlin137789@gmail.com>
2025-12-05 17:03:43 +0800

    修改：     src/app/globals.css 	修改：     src/app/page.tsx     添加live2d模型  public/hiyori_free

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import Wave from './page/compontent/Wave'
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full">
-        <Wave height={100} fillColor="color-mix(in srgb, oklch(95% calc(var(--chromatic-chroma-50) * 0.5) var(--chromatic-hue)) 80%, oklch(100% 0 360))"/>
+    <main className="h-full w-full relative flex flex-col">
+        <Wave className='shrink-0' height={100} fillColor="color-mix(in srgb, oklch(95% calc(var(--chromatic-chroma-50) * 0.5) var(--chromatic-hue)) 80%, oklch(100% 0 360))"/>
+        <div className="w-full h-full shrink grow">
+          11111
+        </div>
       </main>
   );
 }
